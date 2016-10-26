18dfe32bbe4c58717ed8ffde1023bcc0ca4ac598
Skyboom <410733240@qq.com>
2016-10-26 17:50:47 +0800

    1
    1

--- js/open.js
+++ js/open.js
@@ -19,7 +19,7 @@ function Effect(ObjectId,parentId,id){
 function Start(ObjId,method){
 var BoxHeight = $G(ObjId).offsetHeight;         //获取对象高度
 var MinHeight = 5;                  //定义对象最小高度
-var MaxHeight = 224;                 //定义对象最大高度
+var MaxHeight = 460;                 //定义对象最大高度
 var BoxAddMax = 1;                  //递增量初始值
 var Every_Add = 0.15;                //每次的递(减)增量  [数值越大速度越快]
 var Reduce = (BoxAddMax - Every_Add);
